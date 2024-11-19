03da811b288f29ded2ca432d4148b18cb52048a6
yukang <moorekang@gmail.com>
2024-11-19 19:43:01 +0800

    fix bug

--- src/fiber/graph.rs
+++ src/fiber/graph.rs
@@ -584,7 +584,7 @@ where
                 (0, 0)
             } else {
                 let channel_info = self
-                    .get_channel(&route[i + 1].channel_outpoint)
+                    .get_channel(&route[i].channel_outpoint)
                     .expect("channel not found");
                 let channel_update = &if channel_info.node1() == route[i + 1].target {
                     info!(
@@ -782,6 +782,7 @@ where
                         continue;
                     }
                 }
+                info!("now use fee_rate: {:?}", fee_rate);
                 let node = NodeHeapElement {
                     node_id: from,
                     weight,
