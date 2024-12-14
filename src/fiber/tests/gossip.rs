0696a46e88726972df55e82253d50073e48cc7c0
YI <uuuuuu@protonmail.com>
2024-12-14 17:08:35 +0800

    Add unit test to subscribe store updates

--- src/fiber/tests/gossip.rs
+++ src/fiber/tests/gossip.rs
@@ -1,6 +1,6 @@
-use std::str::FromStr;
+use std::{str::FromStr, sync::Arc};
 
-use ractor::{async_trait, concurrency::Duration, ActorRef};
+use ractor::{async_trait, concurrency::Duration, Actor, ActorProcessingErr, ActorRef};
 use tempfile::tempdir;
 use tentacle::{
     builder::ServiceBuilder,
@@ -10,16 +10,16 @@ use tentacle::{
     service::{ServiceError, ServiceEvent},
     traits::ServiceHandle,
 };
-use tokio::spawn;
+use tokio::{spawn, sync::RwLock};
 
 use crate::{
     ckb::tests::actor::create_mock_chain_actor,
     fiber::{
         gossip::{
             ExtendedGossipMessageStore, ExtendedGossipMessageStoreMessage, GossipMessageStore,
-            GossipProtocolHandle,
+            GossipMessageUpdates, GossipProtocolHandle, SubscribableGossipMessageStore,
         },
-        types::BroadcastMessage,
+        types::{BroadcastMessage, BroadcastMessageWithTimestamp},
     },
     gen_rand_node_announcement,
     store::Store,
@@ -65,7 +65,10 @@ async fn run_dummy_tentacle_service(gossip_handle: GossipProtocolHandle) {
     });
 }
 
-async fn create_gossip_store_update_subscriber() -> (
+// This function creates a gossip store that can be subscribed to. The first return value is the
+// the underlying store, the second is the actor that can be used to save messages to the store,
+// The third is an entity from which we can subscribe to store updates.
+async fn create_subscribable_gossip_store() -> (
     Store,
     ActorRef<ExtendedGossipMessageStoreMessage>,
     ExtendedGossipMessageStore<Store>,
@@ -94,10 +97,95 @@ async fn create_gossip_store_update_subscriber() -> (
     )
 }
 
+// This function creates a gossip store that can be subscribed to. The first return value is the
+// the underlying store, the second is the actor that can be used to save messages to the store,
+// The third is an entity from which we can subscribe to store updates. The fourth is a vector
+// that stores all the messages that the subscriber has received.
+async fn create_subscribable_gossip_store_with_subscriber() -> (
+    Store,
+    ActorRef<ExtendedGossipMessageStoreMessage>,
+    ExtendedGossipMessageStore<Store>,
+    Arc<RwLock<Vec<BroadcastMessageWithTimestamp>>>,
+) {
+    let (store, store_actor, store_update_subscriber) = create_subscribable_gossip_store().await;
+    let (subscriber, messages) = Subscriber::start_actor().await;
+    store_update_subscriber
+        .subscribe(None, subscriber, |m| Some(SubscriberMessage::Update(m)))
+        .await
+        .expect("subscribe to store updates");
+    (store, store_actor, store_update_subscriber, messages)
+}
+
+// A subscriber which subscribes to the store updates and save all updates to a vector.
+struct Subscriber {
+    messages: Arc<RwLock<Vec<BroadcastMessageWithTimestamp>>>,
+}
+
+impl Subscriber {
+    fn new() -> Self {
+        Subscriber {
+            messages: Arc::new(RwLock::new(Vec::new())),
+        }
+    }
+
+    async fn start_actor() -> (
+        ActorRef<SubscriberMessage>,
+        Arc<RwLock<Vec<BroadcastMessageWithTimestamp>>>,
+    ) {
+        let subscriber = Subscriber::new();
+        let messages = subscriber.messages.clone();
+        let (actor, _) = Actor::spawn(None, subscriber, ())
+            .await
+            .expect("start subscriber");
+        (actor, messages)
+    }
+}
+
+enum SubscriberMessage {
+    Update(GossipMessageUpdates),
+}
+
+#[async_trait]
+impl Actor for Subscriber {
+    type Msg = SubscriberMessage;
+    type State = ();
+    type Arguments = ();
+
+    async fn pre_start(
+        &self,
+        _: ActorRef<Self::Msg>,
+        _: Self::Arguments,
+    ) -> Result<Self::State, ActorProcessingErr> {
+        Ok(())
+    }
+
+    async fn post_stop(
+        &self,
+        _myself: ActorRef<Self::Msg>,
+        _state: &mut Self::State,
+    ) -> Result<(), ActorProcessingErr> {
+        Ok(())
+    }
+
+    async fn handle(
+        &self,
+        _myself: ActorRef<Self::Msg>,
+        message: Self::Msg,
+        _state: &mut Self::State,
+    ) -> Result<(), ActorProcessingErr> {
+        match message {
+            SubscriberMessage::Update(updates) => {
+                let mut messages = self.messages.write().await;
+                messages.extend(updates.messages);
+            }
+        }
+        Ok(())
+    }
+}
+
 #[tokio::test]
 async fn test_save_gossip_message() {
-    let (store, store_actor, _store_update_subscriber) =
-        create_gossip_store_update_subscriber().await;
+    let (store, store_actor, _store_update_subscriber) = create_subscribable_gossip_store().await;
     let (_, announcement) = gen_rand_node_announcement();
     store_actor
         .send_message(ExtendedGossipMessageStoreMessage::SaveMessage(
@@ -109,4 +197,22 @@ async fn test_save_gossip_message() {
         .get_latest_node_announcement(&announcement.node_id)
         .expect("get latest node announcement");
     assert_eq!(new_announcement, announcement);
+}
+
+#[tokio::test]
+async fn test_subscribe_gossip_store_updates() {
+    let (_, store_actor, _, messages) = create_subscribable_gossip_store_with_subscriber().await;
+    let (_, announcement) = gen_rand_node_announcement();
+    store_actor
+        .send_message(ExtendedGossipMessageStoreMessage::SaveMessage(
+            BroadcastMessage::NodeAnnouncement(announcement.clone()),
+        ))
+        .expect("send message");
+    tokio::time::sleep(Duration::from_millis(200).into()).await;
+    let messages = messages.read().await;
+    assert!(messages.len() == 1);
+    assert_eq!(
+        messages[0],
+        BroadcastMessageWithTimestamp::NodeAnnouncement(announcement)
+    );
 }
