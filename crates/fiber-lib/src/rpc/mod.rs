126e49d21c2529267899f1b386c8aaa43ac0cc16
yukang <moorekang@gmail.com>
2025-07-25 16:39:04 +0800

    try shutdown script

--- crates/fiber-lib/src/rpc/mod.rs
+++ crates/fiber-lib/src/rpc/mod.rs
@@ -158,7 +158,7 @@ pub mod server {
                     .merge(
                         InfoRpcServerImpl::new(
                             network_actor.clone(),
-                            ckb_config.expect("ckb config should be set"),
+                            ckb_config.clone().expect("ckb config should be set"),
                         )
                         .into_rpc(),
                     )
@@ -174,7 +174,12 @@ pub mod server {
             if config.is_module_enabled("channel") {
                 modules
                     .merge(
-                        ChannelRpcServerImpl::new(network_actor.clone(), store.clone()).into_rpc(),
+                        ChannelRpcServerImpl::new(
+                            network_actor.clone(),
+                            store.clone(),
+                            ckb_config.expect("ckb config should be set"),
+                        )
+                        .into_rpc(),
                     )
                     .unwrap();
             }
