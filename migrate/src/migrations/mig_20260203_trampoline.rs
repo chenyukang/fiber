06842ac14f5e334c440e579ddde628a08743353e
yukang <moorekang@gmail.com>
2026-02-08 15:08:33 +0800

    debug migrate

---
+++ migrate/src/migrations/mig_20260203_trampoline.rs
@@ -0,0 +1,55 @@
+use fiber::{
+    store::{migration::Migration, Store},
+    Error,
+};
+use fiber_v061::fiber::channel::ChannelActorState as NewChannelActorState;
+use indicatif::ProgressBar;
+use std::sync::Arc;
+use tracing::info;
+
+// Remember to update the version number here, sample `20311116135521`
+const MIGRATION_DB_VERSION: &str = "20260203152333";
+
+pub struct MigrationObj {
+    version: String,
+}
+
+impl MigrationObj {
+    pub fn new() -> Self {
+        Self {
+            version: MIGRATION_DB_VERSION.to_string(),
+        }
+    }
+}
+
+impl Migration for MigrationObj {
+    fn migrate<'a>(
+        &self,
+        db: &'a Store,
+        _pb: Arc<dyn Fn(u64) -> ProgressBar + Send + Sync>,
+    ) -> Result<&'a Store, Error> {
+        info!(
+            "MigrationObj::migrate to {} ...........",
+            MIGRATION_DB_VERSION
+        );
+
+        info!("migrate ChannelActorState ...");
+        const CHANNEL_ACTOR_STATE_PREFIX: u8 = 0;
+        let prefix = vec![CHANNEL_ACTOR_STATE_PREFIX];
+        for (k, v) in db
+            .prefix_iterator(prefix.as_slice())
+            .take_while(|(col_key, _)| col_key.starts_with(prefix.as_slice()))
+        {
+            let res = bincode::deserialize::<NewChannelActorState>(&v).expect("serialized failed");
+            let payment_hash = res.local_pubkey;
+            eprintln!("payment_hash: {:?}", payment_hash);
+        }
+
+        panic!("migration not implemented yet");
+        Ok(db)
+    }
+
+    fn version(&self) -> &str {
+        &self.version
+    }
+}
